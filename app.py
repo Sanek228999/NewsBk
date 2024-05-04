b39e1877671419ab932761c58d894015bf06cfb0
Sanek228999 <160397304+Sanek228999@users.noreply.github.com>
2024-05-04 18:44:29 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/protected')
-def protected():
-    if 'password' not in request.args:
-        return redirect(url_for('login'))
-    if request.args['password'] != 'your_password':
-        return redirect(url_for('login'))
-    return render_template('protected.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        password = request.form['password']
+        if password == '123':
+            return redirect(url_for('protected'))
+        else:
+            return 'Wrong password. Please try again.'
     return render_template('login.html')
 
+@app.route('/protected')
+def protected():
+    return 'Access granted!'
 
 
 
