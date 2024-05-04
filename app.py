ab83cc2cff670044a3fdd70c31cafd978030ae25
Sanek228999 <160397304+Sanek228999@users.noreply.github.com>
2024-05-04 18:44:12 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask,  request, redirect, url_for, render_template
+import random
 
 app = Flask(__name__)
 
+@app.route('/protected')
+def protected():
+    if 'password' not in request.args:
+        return redirect(url_for('login'))
+    if request.args['password'] != 'your_password':
+        return redirect(url_for('login'))
+    return render_template('protected.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def main():
+    return render_template('main.html')
+
+
+@app.route('/blog')
+def blog():
+    fw = open('news.txt', 'r')
+    news = fw.read().split('\n')
+    fw.close()
+    random.shuffle(news)
+    return render_template('blog.html', news=news)
+
+
+@app.route('/contacts')
+def contact():
+    return render_template('contacts.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
